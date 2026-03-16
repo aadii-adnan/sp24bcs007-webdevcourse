97e45335876123e539a8eb449f64e1fa6c56395b
aadii-adnan <adnanaadi792@gmail.com>
2026-03-16 23:06:37 +0500

    learning js basics

---
+++ Practise/fbjs.js
@@ -0,0 +1,20 @@
+var database = [
+    {
+    username : "adnan",
+    password : "123"
+    }
+]
+
+var newsfeed = [
+    {
+        username: "adnan",
+        timeline:"thsi"
+    }
+]
+
+var usernameprompt = prompt("Enter name");
+var Passwordprompt = prompt("Enter password");
+
+function signin (user,pass){
+    ig
+}
