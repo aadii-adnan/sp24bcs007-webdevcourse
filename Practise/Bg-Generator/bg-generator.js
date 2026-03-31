b01ca5341ecfb0d1d1577a9514d32ac0336c9026
aadii-adnan <adnanaadi792@gmail.com>
2026-03-31 15:34:47 +0500

    Push uptill now

--- Practise/Bg-Generator/bg-generator.js
+++ Practise/Bg-Generator/bg-generator.js
@@ -1,13 +1,14 @@
 const css=document.querySelector("h3");
-const col1 = document.querySelector("color1");
-const col2 = document.querySelector("color2");
-var body =document.querySelector("gradient")
+const col1 = document.querySelector(".color1");
+const col2 = document.querySelector(".color2");
+var body =document.querySelector(".gradient")
+function set_gradient(){
+    body.style.background = `linear-gradient(to right, ${col1.value}, ${col2.value})`;
+    css.textContent=body.style.background + ";";
 
-col1.addEventListener("input",function(){
-    body.style.background = "linear-gradient(to right" + col1.value +"," + col2.value + ")";
+}
 
-})
+col1.addEventListener("input",set_gradient);
 
-col2.addEventListener("input",function(){
+col2.addEventListener("input",set_gradient);
          
-})
